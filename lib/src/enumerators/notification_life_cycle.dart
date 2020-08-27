4ebda9365647024db7e90b1fc12bfea7106ddf96
rafaelsetragni <rafaelsetra@gmail.com>
2020-08-27 10:07:46 -0300

    Documentation in progress

--- lib/src/enumerators/notification_life_cycle.dart
+++ lib/src/enumerators/notification_life_cycle.dart
@@ -1,1 +1,2 @@
+/// Application life cycle at new notification change state
 enum NotificationLifeCycle { Foreground, Background, AppKilled }
--- lib/src/enumerators/notification_privacy.dart
+++ lib/src/enumerators/notification_privacy.dart
@@ -1,1 +1,5 @@
+/// Hides sensitive notifications when the device is on lock screen
+/// [Public]: Show this notification in its entirety on all lockscreens
+/// [Secret]: Do not reveal any part of this notification on a secure lockscreen
+/// [Private]: Show this notification on all lockscreens, but conceal sensitive or private information on secure lockscreens
 enum NotificationPrivacy { Public, Secret, Private }
