3a7e0434ae59636471836c88727e126ffd9ac026
rafaelsetragni <40064496+rafaelsetragni@users.noreply.github.com>
2020-10-22 09:08:54 -0700

    Create AwesomeContentExtension.swift

---
+++ ios/Classes/lib/Services/AwesomeContentExtension.swift
@@ -0,0 +1,12 @@
+//
+//  AwesomeContentExtension.swift
+//  awesome_notifications
+//
+//  Created by Rafael Setragni on 22/10/20.
+//
+
+import Foundation
+
+class AwesomeContentExtension {
+    
+}
