b753931f82e135418970431c28ba3d43220139ce
rafaelsetragni <40064496+rafaelsetragni@users.noreply.github.com>
2020-11-09 21:20:34 -0800

    Update NotificationViewController.swift

--- example/ios/AwesomeContentExtension/NotificationViewController.swift
+++ example/ios/AwesomeContentExtension/NotificationViewController.swift
@@ -5,11 +5,9 @@
 //  Created by Rafael Setragni on 23/10/20.
 //
 
-import UIKit
-import UserNotifications
-import UserNotificationsUI
 import awesome_notifications
 
+@available(iOS 10.0, *)
 class NotificationViewController: AwesomeContentExtension {
 
 }
