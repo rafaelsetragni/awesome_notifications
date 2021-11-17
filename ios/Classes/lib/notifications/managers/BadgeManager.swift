ce82dc160bbf3303813c47634b58283ce500ec59
rafaelsetragni <40064496+rafaelsetragni@users.noreply.github.com>
2021-11-17 15:54:09 -0800

    New permissions class files created

---
+++ ios/Classes/lib/notifications/managers/BadgeManager.swift
@@ -0,0 +1,8 @@
+//
+//  BadgeManager.swift
+//  awesome_notifications
+//
+//  Created by Rafael Setragni on 17/11/21.
+//
+
+import Foundation
---
+++ ios/Classes/lib/notifications/managers/PermissionManager.swift
@@ -0,0 +1,8 @@
+//
+//  PermissionManager.swift
+//  awesome_notifications
+//
+//  Created by Rafael Setragni on 17/11/21.
+//
+
+import Foundation
