bc7d7954fa81cf3b1e24d48e1d57f0a3e4faae9d
Rafael Setragni <113704924+Sl1veRChair@users.noreply.github.com>
2022-10-21 13:58:30 -0300

    Added Simple Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,69 +1,297 @@
-// FIREBASE_MESSAGING will be discontinued
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:awesome_notifications_example/notifications/firebase_controller.dart';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
-import 'package:awesome_notifications_example/routes/routes.dart';
-import 'package:awesome_notifications_example/themes/themes_controller.dart';
-import 'package:awesome_notifications_example/notifications/notifications_controller.dart';
-import 'package:flutter/services.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  NotificationsController.initializeLocalNotifications();
-  App.initialCallAction =
-      await NotificationsController.interceptInitialCallActionRequest();
-
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
-
-  runApp(const App());
+Future<void> main() async {
+  await NotificationController.initializeLocalNotifications();
+  runApp(const MyApp());
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
+class NotificationController {
+  static ReceivedAction? initialAction;
 
-  static ReceivedAction? initialCallAction;
-  static String name = 'Awesome Notifications - Example App';
-  static Color mainColor = const Color(0xFF9D50DD);
+  static Future<void> initializeLocalNotifications() async {
+    await AwesomeNotifications().initialize(
+        'resource://drawable/res_app_icon',//null,//
+        [
+          NotificationChannel(
+              channelKey: 'alerts',
+              channelName: 'Alerts',
+              channelDescription: 'Notification tests as alerts',
+              playSound: true,
+              importance: NotificationImportance.High,
+              defaultPrivacy: NotificationPrivacy.Private,
+              defaultColor: Colors.deepPurple,
+              ledColor: Colors.deepPurple)
+        ],
+        debug: true);
+    initialAction = await AwesomeNotifications().getInitialNotificationAction();
+  }
+
+  static Future<void> startListeningNotificationEvents() async {
+    AwesomeNotifications()
+        .setListeners(onActionReceivedMethod: onActionReceivedMethod);
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> onActionReceivedMethod(
+      ReceivedAction receivedAction) async {
+    MyApp.navigatorKey.currentState?.pushNamedAndRemoveUntil(
+        '/notification-page',
+            (route) =>
+        (route.settings.name != '/notification-page') || route.isFirst,
+        arguments: receivedAction);
+  }
+
+  static Future<bool> displayNotificationRationale() async {
+    bool userAuthorized = false;
+    BuildContext context = MyApp.navigatorKey.currentContext!;
+    await showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: Text('Get Notified!',
+                style: Theme.of(context).textTheme.titleLarge),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'assets/animated-bell.gif',
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                    'Allow Awesome Notifications to send you beautiful notifications!'),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'Deny',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.red),
+                  )),
+              TextButton(
+                  onPressed: () async {
+                    userAuthorized = true;
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'Allow',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.deepPurple),
+                  )),
+            ],
+          );
+        });
+    return userAuthorized &&
+        await AwesomeNotifications().requestPermissionToSendNotifications();
+  }
+
+  static Future<void> createNewNotification() async {
+    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+
+    if (!isAllowed) {
+      isAllowed = await displayNotificationRationale();
+    }
+
+    if (!isAllowed) return;
+
+    await AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: -1, // -1 is replaced by a random number
+            channelKey: 'alerts',
+            title: 'Notification test',
+            body: 'This is just a notification test',
+            bigPicture: 'asset://assets/images/balloons-in-sky.jpg',
+            notificationLayout: NotificationLayout.BigPicture,
+            payload: {'notificationId': '1234567890'}),
+        actionButtons: [
+          NotificationActionButton(key: 'REDIRECT', label: 'Redirect'),
+          NotificationActionButton(
+              key: 'DISMISS',
+              label: 'Dismiss',
+              actionType: ActionType.DismissAction,
+              isDangerousOption: true)
+        ]);
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   // The navigator key is necessary to navigate using static methods
   static final GlobalKey<NavigatorState> navigatorKey =
-      GlobalKey<NavigatorState>();
+  GlobalKey<NavigatorState>();
 
   @override
-  _AppState createState() => _AppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _AppState extends State<App> {
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+
+  static const String routeHome = '/', routeNotification = '/notification-page';
+
   @override
   void initState() {
+    NotificationController.startListeningNotificationEvents();
     super.initState();
+  }
 
-    // Only after at least the action method is set, the notification events are delivered
-    NotificationsController.initializeNotificationsEventListeners();
+  List<Route<dynamic>> onGenerateInitialRoutes(String initialRouteName) {
+    List<Route<dynamic>> pageStack = [];
+    pageStack.add(MaterialPageRoute(
+        builder: (_) =>
+        const MyHomePage(title: 'Awesome Notifications Example App')));
+    if (initialRouteName == routeNotification &&
+        NotificationController.initialAction != null) {
+      pageStack.add(MaterialPageRoute(
+          builder: (_) => NotificationPage(
+              receivedAction: NotificationController.initialAction!)));
+    }
+    return pageStack;
+  }
+
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case routeHome:
+        return MaterialPageRoute(
+            builder: (_) =>
+            const MyHomePage(title: 'Awesome Notifications Example App'));
+
+      case routeNotification:
+        ReceivedAction receivedAction = settings.arguments as ReceivedAction;
+        return MaterialPageRoute(
+            builder: (_) => NotificationPage(receivedAction: receivedAction));
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    String initialRoute =
-        App.initialCallAction == null ? PAGE_HOME : PAGE_PHONE_CALL;
-    debugPrint('initialRoute: $initialRoute');
     return MaterialApp(
-      title: App.name,
-      color: App.mainColor,
-      navigatorKey: App.navigatorKey,
-      initialRoute: initialRoute,
-      routes: materialRoutes,
-      builder: (context, child) => MediaQuery(
-        data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-        child: child ?? const SizedBox.shrink(),
+      title: 'Awesome Notifications - Simple Example',
+      navigatorKey: MyApp.navigatorKey,
+      onGenerateInitialRoutes: onGenerateInitialRoutes,
+      onGenerateRoute: onGenerateRoute,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
-      theme: ThemesController.currentTheme,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Push the button to create a new notification',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => NotificationController.createNewNotification(),
+        tooltip: 'Create New notification',
+        child: const Icon(Icons.outgoing_mail),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({Key? key, required this.receivedAction})
+      : super(key: key);
+
+  final ReceivedAction receivedAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(receivedAction.title ?? receivedAction.body ?? ''),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (receivedAction.bigPictureImage != null)
+              FadeInImage(
+                placeholder: const AssetImage('assets/images/placeholder.gif'),
+                height: MediaQuery.of(context).size.height * .4,
+                width: MediaQuery.of(context).size.width,
+                image: receivedAction.bigPictureImage!,
+                fit: BoxFit.cover,
+              ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                      text: TextSpan(children: [
+                        if (receivedAction.title?.isNotEmpty ?? false)
+                          TextSpan(
+                            text: receivedAction.title!,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        if ((receivedAction.title?.isNotEmpty ?? false) &&
+                            (receivedAction.body?.isNotEmpty ?? false))
+                          TextSpan(
+                            text: '\n\n',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        if (receivedAction.body?.isNotEmpty ?? false)
+                          TextSpan(
+                            text: receivedAction.body!,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                      ]))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black12,
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              child: Text(receivedAction.toString()),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
