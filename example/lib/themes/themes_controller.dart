7513b844fe9e70b9b9400345a2348ee863329e4d
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-02-11 07:58:26 +0100

    brought dependencies inline with the latest versions of flutter and dart

--- example/lib/themes/themes_controller.dart
+++ example/lib/themes/themes_controller.dart
@@ -19,52 +19,15 @@ class ThemesController {
       backgroundColor: Colors.blueGrey.shade400,
 
       appBarTheme: AppBarTheme(
-          // ignore: deprecated_member_use
-          brightness: Brightness.dark,
-          color: Colors.white,
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: App.mainColor,
-          ),
-          // ignore: deprecated_member_use
-          textTheme: TextTheme(
-            headline6: TextStyle(color: App.mainColor, fontSize: 18),
-          )),
-
-      fontFamily: 'Robot',
-
-      // Define the default TextTheme. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-      textTheme: TextTheme(
-        headline1:
-            TextStyle(fontSize: 64.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline2:
-            TextStyle(fontSize: 52.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline3:
-            TextStyle(fontSize: 48.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline4:
-            TextStyle(fontSize: 32.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline5:
-            TextStyle(fontSize: 28.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline6:
-            TextStyle(fontSize: 22.0, height: 1.5, fontWeight: FontWeight.w500),
-        subtitle1:
-            TextStyle(fontSize: 18.0, height: 1.5, color: Colors.black54),
-        subtitle2:
-            TextStyle(fontSize: 12.0, height: 1.5, color: Colors.black54),
-        button: TextStyle(fontSize: 16.0, height: 1.5, color: Colors.black54),
-        bodyText1: TextStyle(fontSize: 16.0, height: 1.5),
-        bodyText2: TextStyle(fontSize: 16.0, height: 1.5),
+        // ignore: deprecated_member_use
+        brightness: Brightness.dark,
+        color: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: App.mainColor,
+        ),
       ),
-
-      buttonTheme: ButtonThemeData(
-        buttonColor: Colors.grey.shade200,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5))),
-        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        textTheme: ButtonTextTheme.accent,
-      ),
-    )
+    ),
   };
 
   static ThemeData? _currentTheme;
