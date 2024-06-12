835447e4aa50eae52ae4615151d42722f42b6db7
Eyad Ahmed <iyadahmed430@gmail.com>
2024-06-12 09:30:32 +0300

    Fix app crash due to use of obsolete Flutter properties
    https://docs.flutter.dev/release/breaking-changes/3-19-deprecations

--- example/lib/themes/themes_controller.dart
+++ example/lib/themes/themes_controller.dart
@@ -12,13 +12,12 @@ class ThemesController {
       primaryColor: App.mainColor,
       // ignore: deprecated_member_use
       colorScheme: const ColorScheme.light().copyWith(
+        surface: Colors.blueGrey.shade400,
         secondary: Colors.blueGrey,
       ),
       canvasColor: Colors.white,
       focusColor: Colors.blueAccent,
       disabledColor: Colors.grey,
-
-      backgroundColor: Colors.blueGrey.shade400,
 
       appBarTheme: AppBarTheme(
           color: Colors.white,
@@ -33,25 +32,25 @@ class ThemesController {
       // Define the default TextTheme. Use this to specify the default
       // text styling for headlines, titles, bodies of text, and more.
       textTheme: const TextTheme(
-        headline1:
+        displayLarge:
             TextStyle(fontSize: 64.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline2:
+        displayMedium:
             TextStyle(fontSize: 52.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline3:
+        displaySmall:
             TextStyle(fontSize: 48.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline4:
+        headlineMedium:
             TextStyle(fontSize: 32.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline5:
+        headlineSmall:
             TextStyle(fontSize: 28.0, height: 1.5, fontWeight: FontWeight.w500),
-        headline6:
+        titleLarge:
             TextStyle(fontSize: 22.0, height: 1.5, fontWeight: FontWeight.w500),
-        subtitle1:
+        titleMedium:
             TextStyle(fontSize: 18.0, height: 1.5, color: Colors.black54),
-        subtitle2:
+        titleSmall:
             TextStyle(fontSize: 12.0, height: 1.5, color: Colors.black54),
-        button: TextStyle(fontSize: 16.0, height: 1.5, color: Colors.black54),
-        bodyText1: TextStyle(fontSize: 16.0, height: 1.5),
-        bodyText2: TextStyle(fontSize: 16.0, height: 1.5),
+        labelLarge: TextStyle(fontSize: 16.0, height: 1.5, color: Colors.black54),
+        bodyLarge: TextStyle(fontSize: 16.0, height: 1.5),
+        bodyMedium: TextStyle(fontSize: 16.0, height: 1.5),
       ),
 
       buttonTheme: ButtonThemeData(
